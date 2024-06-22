5f4a98b36e0e13b509149bc914e00ffb3e3aa13e
J∞rdan 3 <147287469+jordan123Fun@users.noreply.github.com>
2024-06-21 19:47:05 -0500

    Update server.py

--- server.py
+++ server.py
@@ -5,7 +5,7 @@ import os
 app = Flask(__name__)
 
 # Configure the upload folder and allowed file extensions
-UPLOAD_FOLDER = '/path/to/upload/folder'
+UPLOAD_FOLDER = 'Files'
 ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
@@ -15,7 +15,7 @@ def allowed_file(filename):
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return "<iframe width="560" height="315" src="https://www.youtube.com/embed/dQw4w9WgXcQ?si=ohUyfHvMMsP5doM1&amp;controls=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>"
 
 @app.route('/upload', methods=['GET', 'POST'])
 def upload_file():
